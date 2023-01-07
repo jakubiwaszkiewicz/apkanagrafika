55ac0847f7a0890beacb148beac3bfb7978e98ac
jakubiwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-01-07 21:50:20 +0100

    aaaa

---
+++ src/components/content/Content.js
@@ -0,0 +1,22 @@
+function Content() {
+    return (
+        <div className="Content">
+            <section id="homepage">
+                <h1>Sieeeemano</h1>
+                <h1>Witam w mojej apce</h1>
+                <p>Sorki za taką apke ale nie mam za wiele czasu na jej wykonanie więc jest w formie możliwie
+                    najprostszej i najszybszej dla mnie do wykonania, co nie zmienia faktu że motyw przewodni
+                    moich apek czyli orginalność zostaje zachowana</p>
+                <p>Żeby zdjęcia w apce się ładnie wyświtlały polecam odpalić ją na kompie bo niestety wersja mobilna
+                    kosztowała by mnie z pare godzin pracy więcej :(</p>
+                <img ></img>
+            </section>
+            <section id="dlaczegoja"></section>
+            <section id="motywacja"></section>
+            <section id="dlaczegobse"></section>
+            <section id="meme"></section>
+        </div>
+    );
+}
+
+export default Content;
